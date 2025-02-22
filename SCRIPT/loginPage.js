025c649ca699febbe85c94619e19c0c2927fde4d
manveethjain8 <manveethjain@gmail.com>
2025-02-22 19:20:18 +0530

    update

---
+++ SCRIPT/loginPage.js
@@ -0,0 +1,48 @@
+console.log(localStorage);
+let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+console.log(accounts);
+
+function loginFunction(){
+    document.querySelector('.enteredEmail').classList.remove('invalidEmail');
+    document.querySelector('.enteredPassword').classList.remove('invalidPassword');
+
+
+    let enteredEmail = document.querySelector('.enteredEmail').value;
+    let enteredPassword = document.querySelector('.enteredPassword').value;
+
+    let accountFound = accounts.find(account => enteredEmail === account.email);
+
+    if (accountFound) {
+        if (accountFound.password === enteredPassword) {
+            window.location.href = './index.html';
+        } else {
+            document.querySelector('.enteredPassword').classList.add('invalidPassword');
+        }
+    } else {
+        document.querySelector('.enteredEmail').classList.add('invalidEmail');
+    }
+}
+
+document.querySelector('.loginButton').addEventListener('click', () => {
+    loginFunction();
+});
+
+
+
+document.addEventListener('keydown',(event)=>{
+    if(event.key==="Enter"){
+        loginFunction();
+    }
+});
+
+document.querySelector('.signUpButton').addEventListener('click',()=>{
+    window.location.href='./signupPage.html';
+});
+
+document.querySelector('.enteredEmail').addEventListener('keydown', () => {
+    document.querySelector('.enteredEmail').classList.remove('invalidEmail');
+});
+
+document.querySelector('.enteredPassword').addEventListener('keydown', () => {
+    document.querySelector('.enteredPassword').classList.remove('invalidPassword');
+});
---
+++ SCRIPT/signupPage.js
@@ -0,0 +1,55 @@
+console.log(localStorage);
+let accounts = JSON.parse(localStorage.getItem('accounts')) || [];
+
+
+function signUpFunction(){
+        document.querySelector('.enteredEmail').classList.remove('invalidEmail');
+    
+        let enteredEmail = document.querySelector('.enteredEmail').value;
+        let enteredPassword = document.querySelector('.enteredPassword').value;
+
+        if(enteredEmail === '' || enteredPassword === ''){
+            document.querySelector('.enteredEmail').classList.add('invalidEmail');
+            document.querySelector('.enteredPassword').classList.add('invalidEmail');
+        }
+    
+        let accountFound = accounts.find(account => enteredEmail === account.email);
+    
+        if (accountFound) {
+            document.querySelector('.enteredEmail').classList.add('invalidEmail');
+        }else{
+            let newAccount={email:enteredEmail, password:enteredPassword};
+            accounts.push(newAccount);
+            localStorage.setItem('accounts',JSON.stringify(accounts));
+            alert("Account is created successfully.You will be redirected to the login page");
+            window.location.href="./index.html";
+        }
+    console.log(localStorage);
+    console.log(accounts);
+}
+
+document.querySelector('.enteredEmail').addEventListener('keydown', () => {
+    document.querySelector('.enteredEmail').classList.remove('invalidEmail');
+});
+
+document.querySelector('.enteredPassword').addEventListener('keydown', () => {
+    document.querySelector('.enteredPassword').classList.remove('invalidEmail');
+});
+
+document.addEventListener('keydown',(event)=>{
+    if(event.key==='/'){
+        localStorage.removeItem('accounts');
+        accounts=[];
+        alert("All the accounts are cleared")
+    }
+});
+
+document.addEventListener('keydown',(event)=>{
+    if(event.key==="Enter"){
+        signUpFunction();
+    }
+});
+
+document.querySelector('.signUpButton').addEventListener('click',()=>{
+    signUpFunction();
+} );
